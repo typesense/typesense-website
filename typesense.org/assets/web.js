efd95e2b6fcdcdcb64669d34a5a539292f8c83b2
Jason Bosco <mail@jasonbos.co>
2020-09-08 22:44:25 -0700

    Use Typesense Cloud cluster for home page demo

--- typesense.org/assets/web.js
+++ typesense.org/assets/web.js
@@ -11,13 +11,13 @@ $(document).ready(function() {
         per_page: 4
     };
 
-    var search_api_key = 'YhdsuG$Gs72NSNyegdbsa^hTh';
+    var search_api_key = 'UPuUG3rjIQD7hb2TNHNNMyaKRj6OPsKN';
 
     $("#goodreads-search input").select();
     $("#goodreads-search input").focus();
 
     function getResults(q, callback) {
-        var url = "https://t1m.typesense.org/collections/books/documents/search?q="+ q +
+        var url = "https://jqeda7gwobunm1s4.a1.typesense.net/collections/books/documents/search?q="+ q +
             "&prefix=true&query_by=title&sort_by=ratings_count:DESC&page="+search_state.page+"&per_page=" +
             search_state.per_page + "&num_typos=2";
 
