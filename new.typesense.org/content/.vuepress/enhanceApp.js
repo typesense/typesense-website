2a54c17696f4d15fdcd87c145da49a5eb09bfbea
Jason Bosco <mail@jasonbos.co>
2021-02-22 23:15:37 -0800

    Add bootstrap

--- new.typesense.org/content/.vuepress/enhanceApp.js
+++ new.typesense.org/content/.vuepress/enhanceApp.js
@@ -3,6 +3,8 @@ import 'prismjs/themes/prism-tomorrow.css'
 import Prism from 'vue-prism-component'
 import Vuex from 'vuex'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './theme/styles/bootstrap_overrides.scss'
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
@@ -14,4 +16,7 @@ export default ({
   Vue.component('prism', Prism)
   Vue.use(Vuex)
   Vue.mixin({ store: store })
+
+  Vue.use(BootstrapVue)
+  Vue.use(IconsPlugin)
 }
