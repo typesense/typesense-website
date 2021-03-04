d508b29427a4ce3c9cc13b945782b93d44076417
Jason Bosco <mail@jasonbos.co>
2021-03-03 17:39:29 -0800

    Fonts

--- new-site/docs-site/content/.vuepress/config.js
+++ new-site/docs-site/content/.vuepress/config.js
@@ -22,6 +22,7 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'icon', href: '/favicon.png' }],
   ],
 
   /**
