eda7395343302c8557188c4a156ecc23bbe50b70
Jason Bosco <mail@jasonbos.co>
2021-03-09 13:12:43 -0800

    Testing IP

--- new-site/typesense.org/nuxt.config.js
+++ new-site/typesense.org/nuxt.config.js
@@ -74,7 +74,7 @@ const config = {
   'google-gtag': {
     id: 'UA-116415641-1',
     config: {
-      anonymize_ip: true, // anonymize IP
+      anonymize_ip: false, // anonymize IP
       send_page_view: false, // might be necessary to avoid duplicated page track on page reload
       linker: {
         domains: [
