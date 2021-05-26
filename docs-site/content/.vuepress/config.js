3ca43c5ab2488e626f6b60c402566d48b9eb8280
Jason Bosco <mail@jasonbos.co>
2021-05-26 12:01:25 -0700

    Remove back to top plugin since it messes up scrolling to anchor links

--- docs-site/content/.vuepress/config.js
+++ docs-site/content/.vuepress/config.js
@@ -737,7 +737,7 @@ let config = {
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
-    '@vuepress/plugin-back-to-top',
+    // '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
     ['@dovyp/vuepress-plugin-clipboard-copy', true],
     require('./plugins/typesense-enhancements'),
