ae6bb97b8906af0a84b6200cba7afe6cc94faa02
Jason Bosco <mail@jasonbos.co>
2021-06-29 17:53:18 -0700

    Avoid loading Prism twice, breaks navigation on pages with SSR code blocks.

--- docs-site/content/.vuepress/plugins/enhanceApp.js
+++ docs-site/content/.vuepress/plugins/enhanceApp.js
@@ -6,9 +6,6 @@
  * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
  */
 
-import 'prismjs'
-import 'prismjs/themes/prism-tomorrow.css'
-import Prism from 'vue-prism-component'
 import Vuex from 'vuex'
 import VueGtag from 'vue-gtag'
 
@@ -47,7 +44,6 @@ export default ({
   siteData, // site metadata
   isServer, // is this enhancement applied in server-rendering or client
 }) => {
-  Vue.component('Prism', Prism)
   Vue.use(Vuex)
 
   Vue.use(VueGtag, {
