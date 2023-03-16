a0fd093e3b51eab14d3a586bdfe3fd59f1565d4a
Jason Bosco <mail@jasonbos.co>
2023-03-15 19:23:31 -0500

    Tweaks to download page

--- typesense.org/components/Downloads/SectionHero.vue
+++ typesense.org/components/Downloads/SectionHero.vue
@@ -5,19 +5,27 @@
         <div class="col-sm">
           <div class="h4 mb-0 mb-2">⚡ 🔍</div>
           <h1 class="display-4">Download Typesense</h1>
-          <div class="lead">
-            We offer the following pre-built artifacts to run Typesense in
-            Production or locally.
-          </div>
-          <div class="lead mt-1">
-            We also offer a hosted version of Typesense called
-            <a href="https://cloud.typesense.org" target="_blank"
-              >Typesense Cloud</a
-            >.
-          </div>
-          <div class="lead mt-5">
-            Once you’ve downloaded Typesense, read the documentation to
-            <a href="/docs/">Get Started</a>.
+          <div class="row">
+            <div class="col-sm-9">
+              <div class="lead">
+                Typesense is an open source search engine that can be downloaded
+                and run from your computer locally or from any server. We also
+                offer a hosted version of Typesense called
+                <a href="https://cloud.typesense.org" target="_blank"
+                  >Typesense Cloud</a
+                >.
+              </div>
+              <div class="lead mt-4">
+                To download Typesense,
+                <strong>click on one of the links below</strong> or use the
+                <strong>terminal commands</strong> shown, depending on your
+                operating system and CPU architecture.
+              </div>
+              <div class="lead mt-4">
+                Once you’ve downloaded Typesense, read the documentation to
+                <a href="/docs/">Get Started</a>.
+              </div>
+            </div>
           </div>
         </div>
       </div>
