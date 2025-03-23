894066de2b6f77e4ba0c0a54dc6d51644e505b45
Hayden <hayden.hhoang@gmail.com>
2025-03-23 23:43:35 +0700

    add links to each feature in HomeSectionFeatures

--- typesense.org-v3/components/Home/SectionFeatures.vue
+++ typesense.org-v3/components/Home/SectionFeatures.vue
@@ -6,6 +6,7 @@ const features = [
     description:
       "Spellng Mistakes? Not a problem. Typesense automatically tries to correct typos.",
     icon: "/images/features/typography.svg",
+    link: "https://typesense.org/docs/latest/api/search.html#typo-tolerance-parameters",
   },
   {
     name: "Tunable Ranking",
@@ -13,6 +14,7 @@ const features = [
     description:
       "Tailor your results to perfection via flexible and fast query-time ranking.",
     icon: "/images/features/leaderboard.svg",
+    link: "https://typesense.org/docs/guide/ranking-and-relevance.html",
   },
   {
     name: "Synonyms",
@@ -20,6 +22,7 @@ const features = [
     description:
       "Banish “pants vs. trousers” confusion. Map related terms so users always find what they’re after.",
     icon: "/images/features/circles.svg",
+    link: "https://typesense.org/docs/latest/api/synonyms.html",
   },
   {
     name: "Multi-Tenant API Keys",
@@ -27,6 +30,7 @@ const features = [
     description:
       "Manage data for multiple users in a single collection, and manage aCLs for them within Typesense.",
     icon: "/images/features/door-key.svg",
+    link: "https://typesense.org/docs/latest/api/api-keys.html",
   },
   {
     name: "Dynamic Sorting",
@@ -34,6 +38,7 @@ const features = [
     description:
       "Let users reorder results on the fly at Runtime, without having to duplicate data.",
     icon: "/images/features/sort-arrow.svg",
+    link: "https://typesense.org/docs/latest/api/search.html#sort-results",
   },
   {
     name: "Merchandising",
@@ -41,6 +46,7 @@ const features = [
     description:
       "Pin specific records in a particular position to feature or merchandize them.",
     icon: "/images/features/paper-pin.svg",
+    link: "https://typesense.org/docs/guide/ranking-and-relevance.html#promoting-or-hiding-results-merchandising",
   },
   {
     name: "Grouping & Distinct",
@@ -48,6 +54,7 @@ const features = [
     description:
       "Prevent duplicate overload. Group Similar Variants and keep your Results neat and tidy.",
     icon: "/images/features/list.svg",
+    link: "https://typesense.org/docs/latest/api/search.html#group-results",
   },
   {
     name: "Filtering & Faceting",
@@ -55,6 +62,7 @@ const features = [
     description:
       "Empower users to slice and dice results by Attributes. Quick and precise.",
     icon: "/images/features/sort.svg",
+    link: "https://typesense.org/docs/latest/api/search.html#filter-results",
   },
   {
     name: "Geo Search",
@@ -62,6 +70,7 @@ const features = [
     description:
       "Serve up location-based results. Perfect for local listings, store finders, or city-specific content.",
     icon: "/images/features/geotargeting.svg",
+    link: "https://typesense.org/docs/latest/api/geosearch.html",
   },
   {
     name: "Federated Search",
@@ -69,6 +78,7 @@ const features = [
     description:
       "Aggregate data from multiple sources into one search box. One query, many streams of information.",
     icon: "/images/features/emblem.svg",
+    link: "https://typesense.org/docs/latest/api/federated-multi-search.html",
   },
   {
     name: "Vector & Semantic Search",
@@ -76,6 +86,7 @@ const features = [
     description:
       "Go beyond keywords. Match the meaning behind each query and deliver eerily accurate results.",
     icon: "/images/features/semantic.svg",
+    link: "https://typesense.org/docs/latest/api/vector-search.html",
   },
   {
     name: "Easy High Availability",
@@ -83,6 +94,7 @@ const features = [
     description:
       "Stay online, even when hardware hiccups happen. replication keeps your search up and running.",
     icon: "/images/features/ok.svg",
+    link: "https://typesense.org/docs/guide/high-availability.html",
   },
 ];
 </script>
@@ -102,43 +114,52 @@ const features = [
       </h2>
     </div>
 
-    <Card
-      class="!h-auto p-4 max-sm:bg-bg max-sm:p-0.5 max-sm:pb-2.5 max-sm:shadow-[-4px_4px_16px_0px_rgba(0,_0,_0,_0.07)]"
+    <CustomLink
+      class="h-full"
       v-for="item in features"
+      :key="item.name"
+      :to="item.link"
     >
-      <div class="flex h-full w-full flex-col">
-        <div
-          class="relative aspect-[40/17] overflow-hidden max-sm:aspect-auto max-sm:h-[108px]"
-        >
-          <img
-            class="absolute w-full rounded-2xl max-sm:min-w-[145%] max-sm:rounded-md"
-            :src="item.image"
-            :alt="item.name"
-          />
+      <Card
+        class="!h-full p-4 max-sm:bg-bg max-sm:p-0.5 max-sm:pb-2.5 max-sm:shadow-[-4px_4px_16px_0px_rgba(0,_0,_0,_0.07)]"
+      >
+        <div class="flex h-full w-full flex-col">
           <div
-            class="absolute right-0 top-0 grid size-12 place-items-center rounded-xl bg-secondary"
+            class="relative aspect-[40/17] overflow-hidden max-sm:aspect-auto max-sm:h-[108px]"
           >
-            <img :src="item.icon" alt="" />
+            <img
+              class="absolute w-full rounded-2xl max-sm:min-w-[145%] max-sm:rounded-md"
+              :src="item.image"
+              :alt="item.name"
+            />
+            <div
+              class="absolute right-0 top-0 grid size-12 place-items-center rounded-xl bg-secondary"
+            >
+              <img :src="item.icon" alt="" />
+            </div>
+          </div>
+          <div class="mt-4 max-sm:mx-1.5 max-sm:mt-2.5">
+            <h3>{{ item.name }}</h3>
+            <p class="subtext !text-[14px] max-sm:mt-1 max-sm:!text-[12px]">
+              {{ item.description }}
+            </p>
           </div>
         </div>
-        <div class="mt-4 max-sm:mx-1.5 max-sm:mt-2.5">
-          <h3>{{ item.name }}</h3>
-          <p class="subtext !text-[14px] max-sm:mt-1 max-sm:!text-[12px]">
-            {{ item.description }}
-          </p>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </CustomLink>
     <div id="empty" class="max-sm:hidden"></div>
 
-    <div class="relative rounded-2xl bg-secondary p-4 max-sm:hidden">
+    <CustomLink
+      to="https://typesense.org/docs/overview/features.html"
+      class="relative rounded-2xl bg-secondary p-4 max-sm:hidden"
+    >
       <h3 class="!text-2xl !tracking-[-0.72px]">Typesense <br />Features</h3>
       <img
         class="absolute bottom-0 right-0"
         src="/images/features/tasks-checked.svg"
         alt="Typesense Features"
       />
-    </div>
+    </CustomLink>
   </section>
 </template>
 
